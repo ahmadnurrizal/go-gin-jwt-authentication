c82d96380c617c54d5e2c929d6ccc6528fcc547a
ahmadnur.rizal <54085895+ahmadnurrizal@users.noreply.github.com>
2023-03-07 10:17:03 +0700

    Creating JWT Authentication Middleware

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/ahmadnurrizal/go-gin-jwt-authentication/controllers"
+	"github.com/ahmadnurrizal/go-gin-jwt-authentication/middlewares"
 	"github.com/ahmadnurrizal/go-gin-jwt-authentication/models"
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +17,10 @@ func main() {
 
 	public.POST("/register", controllers.Register)
 	public.POST("/login", controllers.Login)
+
+	protected := r.Group("/api/admin")
+	protected.Use(middlewares.JwtAuthMiddleware())
+	protected.GET("/user", controllers.CurrentUser)
 
 	r.Run("localhost:8080")
 
